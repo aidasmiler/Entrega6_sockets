3aa23ec82083626a8216554ab27c7793f185a818
Aída Rodríguez <63601227+aidasmiler@users.noreply.github.com>
2021-04-21 16:55:22 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -4,17 +4,51 @@ const quiz = require("./cmds_quiz.js");
 const favs = require("./cmds_favs.js");
 const readline = require('readline');
 
+let net = require('net');
+let port = process.argv[3] || 8080;
+let host = process.argv[2] || "localhost";
+
+//let socket = net.createConnection(port, host);
+
+var server = net.createServer((socket) => {
+  /*socket.write('Welcome (Echo server)\n');
+  socket.on('data', function(data){
+    socket.write(data);
+  });*/
+  socket.on('connect', function () {
+    console.log("Server at port: " + port + " of " + host);    
+  })
+
+
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+  input: socket,
+  output: socket,
   prompt: "> "
 });
-rl.log = (msg) => console.log(msg);  // Add log to rl interface
+
+rl.log = (msg) => socket.write(msg);  // Add log to rl interface
 rl.questionP = function (string) {   // Add questionP to rl interface
   return new Promise ( (resolve) => {
     this.question(`  ${string}: `, (answer) => resolve(answer.trim()))
   })
 };
+
+/*
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "> "
+});
+*/
+
+/*rl.log = (msg) => console.log(msg);  // Add log to rl interface
+//rl.log = (msg) => socket.write(msg+"\n");
+rl.questionP = function (string) {   // Add questionP to rl interface
+  return new Promise ( (resolve) => {
+    this.question(`  ${string}: `, (answer) => resolve(answer.trim()))
+  })
+};
+*/
 
 rl.prompt();
 
@@ -41,11 +75,13 @@ rl.on('line', async (line) => {
     else if (['cf', 'fc'].includes(cmd))      { await favs.create(rl);}
     else if (['df', 'fd'].includes(cmd))      { await favs.delete(rl);}
 
-    else if ('e'===cmd)  { rl.log('Bye!'); process.exit(0);}
+    else if ('e'===cmd)  { rl.log('Bye!');/* process.exit(0);*/ socket.destroy();}
     else                 {  rl.log('UNSUPPORTED COMMAND!');
                             user.help(rl);
                          };
     } catch (err) { rl.log(`  ${err}`);}
     finally       { rl.prompt(); }
   });
-
+});
+  server.listen(port);
+ 
